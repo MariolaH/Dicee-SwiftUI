67d5e93ca03a37f855b8955901b2db745c87a1ab
Mariola <mariolahullings@gmail.com>
2024-01-30 13:13:48 -0500

    Added functionality

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@State - makes the property mutable - allow us to update the variables
+    //In swift Structures are immutable
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
     var body: some View {
         ZStack {
             Image("background")
@@ -16,22 +20,31 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
             VStack {
                 Image("diceeLogo")
+                    .padding(.top)
                 Spacer()
                 HStack {
-                    DiceView(n: 1)
-                    DiceView(n: 2)
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
                 }
                 .padding(.horizontal)
                 Spacer()
                 Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                    
                 }) {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.yellow)
+                        .blendMode(.screen)
+                        .frame(width: 130, height: 130)
+                        .overlay(
                     Text("Roll")
                         .font(.system(size: 50))
                         .fontWeight(.bold)
                         .foregroundColor(Color.white)
                         .padding(.horizontal)
+                        )
                 }
-                .background(Color.red)
                 .padding(.bottom)
 
             }
@@ -43,9 +56,10 @@ struct DiceView: View {
     let n: Int
     var body: some View {
         Image("dice\(n)")
+            .resizable()
+            .frame(width: 145, height: 145)
             .padding()
-//            .resizable()
-//            .aspectRatio(1, contentMode: .fit)
+        
     }
 }
 
